d0d4975d5f0c666c50ee81f2f6d92b8a7c7ec838
sarkerzaman <sarkerzaman@gmail.com>
2023-09-02 00:36:54 +0600

    Git repo make my life hell

--- module9/vue3-blog-assignment/src/App.vue
+++ module9/vue3-blog-assignment/src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue';
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template>  
+    <header>
+      <Navigation/>
+    </header>
+    <main>
+      <router-view></router-view>
+      <!-- The component associated with the current route will be rendered here -->
+    </main>
+    <footer>
+      <!-- Your footer content can be here -->
+    </footer> 
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#app {
+  text-align: center;
+  margin-top: 20px;
 }
 </style>
