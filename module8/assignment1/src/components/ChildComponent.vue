6a4d71ff693b883b05ffdf37fa6ed3d61441674d
sarkerzaman <sarkerzaman@gmail.com>
2023-09-01 23:55:45 +0600

    blog project done

--- module8/assignment1/src/components/ChildComponent.vue
+++ module8/assignment1/src/components/ChildComponent.vue
@@ -1,20 +1,17 @@
 <script setup>
-import { ref, defineEmits } from 'vue';
+import { ref, defineProps, defineEmits } from 'vue';
 
-const { emit } = defineEmits(['messageEmitted']);
+defineProps(['message']);
+defineEmits(['update:message']);
 
-const message = ref('');
+// const message = ref('Custom message from child!');
 
-const emitMessage = () => {
-    message.value = 'Custom message from child!';
-    emit('messageEmitted', message.value);
-};
 </script>
 <template>
 <div class="bg-green p-2 border border-gray-300">
     <h2 class="text-lg font-semibold mb-2">Child Component</h2>
     <p>Received Message: {{ message }}</p>
-    <button @click="emitMessage"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button @click="$emit('update:message', $event.target.value)"  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Click for Emit Message
     </button>
 </div>
--- module8/assignment1/src/components/ParentComponent.vue
+++ module8/assignment1/src/components/ParentComponent.vue
@@ -2,12 +2,10 @@
 import { defineProps, ref } from 'vue';
 import ChildComponent from './ChildComponent.vue';
 
-defineProps(['parentMessage']);
-
-const emittedMessage = ref('Default message from parent');
+const emittedMessage = ref({ message: 'Default message from parent' });
 
 const handleMessageEmitted = (message) => {
-  emittedMessage.value = message;
+  emittedMessage.message = message;
 };
 </script>
 <template>
@@ -15,9 +13,10 @@ const handleMessageEmitted = (message) => {
   <h1 class="my-10">Parent Component</h1>
   <p class="my-5">
     <h2 class="text-xl">Message in Parent</h2>
-    Emitted Message: {{ emittedMessage }}
+    Emitted Message: {{ emittedMessage.message }}
+    
   </p>
 
-  <ChildComponent :message="parentMessage" @messageEmitted="handleMessageEmitted" />
+  <ChildComponent v-model:message="emittedMessage.message" @messageEmitted="handleMessageEmitted" />
   </section>
 </template>
